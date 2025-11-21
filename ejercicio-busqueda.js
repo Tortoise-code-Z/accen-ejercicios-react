c435034c273855995a5cfb786b3fbdadef90b6a5
Victor Perez <tortoise.code@protonmail.com>
2025-11-21 20:24:43 +0000

    .

--- ejercicio-busqueda.js
+++ ejercicio-busqueda.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Busqueda from "./Components/Busqueda.jsx";
+import Busqueda from "./src/Components/Busqueda.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  React.createElement(React.StrictMode, null, React.createElement(Busqueda))
+    React.createElement(React.StrictMode, null, React.createElement(Busqueda))
 );
